07fcfb4a8363dc47b1b01c02c52f74697417d5ce
Leticiamoraiis/Codifica-mais <leticia2016morais@gmail.com>
2026-02-24 20:02:21 -0300

    correção

--- PHP-ESTOQUE/database.php
+++ PHP-ESTOQUE/database.php
@@ -3,7 +3,7 @@
 $host = 'localhost';
 $db   = 'gestao_estoque';
 $user = 'root';
-$pass = 'Adidesba146*.';
+$pass = '';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
